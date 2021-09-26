d0d1abdddfcff8b153ef5d42480ffde8621d47e6
lotfy1654 <lotfy1654@gmail.com>
2021-09-27 01:12:03 +0200

    Finish

--- JS/index.js
+++ JS/index.js
@@ -1,27 +1,19 @@
-$(function() {
+$(function () {
+  "use strict";
+  // Adjust Slider Height
+  var winH = $(window).height(),
+    upperH = $(".upper-bar").innerHeight(),
+    navH = $(".nabbar").innerHeight();
 
-            'use strict';
-            // Adjust Slider Height
-            var winH= $(window).height(),
-                upperH = $('.upper-bar').innerHeight(),
-                navH = $('.nabbar').innerHeight();
+  $(".slider").height(winH - (upperH + navH));
 
-            $('.slider').height(winH - (upperH + navH));
-
-
-            $('.featured-work ul li').on('click', function() {
-                    $(this).addclass('active').siblings().removeclass('active');
-                    if ($(this).data('class') === 'all') {
-                        $('.shuffle-imgs .col-md').css('opacity', '1')
-                    } else {
-                        $('.shuffle-imgs .col-md').css('opacity', '.08')
-                        $(this).data('class').parent().css('opacity', '1')
-                        }
-
-
-                    });
-
-
-
-
-            })
+  $(".featured-work ul li").on("click", function () {
+    $(this).addclass("active").siblings().removeclass("active");
+    if ($(this).data("class") === "all") {
+      $(".shuffle-imgs .col-md").css("opacity", "1");
+    } else {
+      $(".shuffle-imgs .col-md").css("opacity", ".08");
+      $(this).data("class").parent().css("opacity", "1");
+    }
+  });
+});
